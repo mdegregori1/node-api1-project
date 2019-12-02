a6f9bb22031f72b8660e1eacbf4bfd635dfba550
Mauricio Degregori <degregori2@gmail.com>
2019-12-02 16:10:33 -0600

    user get request working

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 // implement your API here
 const express = require('express');
 
+const db = require('./data/db.js');
+
 const server = express();
+
+server.use(express.json());
 
 server.get('/', (req, res) => {
     res.send({api: 'If this prints out, API is running'})
+})
+
+// post to /api/users
+
+// server.post('/api/users', (req, res) => {
+//     const data = req.body;
+//     db.insert(data)
+//     .then( users =>{
+//         res.status(201).json(users);
+//     })
+//     .catch( error => {
+//         console.log('error on post for users', error);
+//         res.status(500).json({errorMessage: 'jfdsflksd'})
+//     })
+
+// })
+
+//get from /api/users
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then( users =>{
+        res.status(200).json(users)
+    })
+    .catch( error => {
+        console.log('error on GET users', error);
+        res.status(500).json({ error: 'Error getting list of user'})
+    })
 })
 
 
