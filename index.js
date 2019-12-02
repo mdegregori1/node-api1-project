7f2ada1c2d6375c4fdb6063c1daeb29b65e075fb
Mauricio Degregori <degregori2@gmail.com>
2019-12-02 17:12:18 -0600

    get request for users/:id working

--- index.js
+++ index.js
@@ -43,6 +43,37 @@ server.get('/api/users', (req, res) => {
     })
 })
 
+// get from /api/users/:id
+server.get('/api/users/:id',(req, res) => {
+    const id = req.params.id;
+    db.findById(id)
+    .then( id => {
+        if (id) {
+            res.status(200).json(id)
+        } else {
+            res.status(404).json({message: "The user with the specified ID does not exist."})
+        }
+    })
+    .catch( error => {
+        console.log(error)
+        res.status(500).json({ error: "The user information could not be retrieved"})
+    })
+
+})
+
+
+// When the client makes a GET request to /api/users/:id:
+
+// If the user with the specified id is not found:
+
+// return HTTP status code 404 (Not Found).
+// return the following JSON object: { message: "The user with the specified ID does not exist." }.
+// If there's an error in retrieving the user from the database:
+
+// cancel the request.
+// respond with HTTP status code 500.
+// return the following JSON object: { error: "The user information could not be retrieved.
+
 
 const port = 5000;
 server.listen(port, () => 
