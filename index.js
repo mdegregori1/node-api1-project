5e2f372c06ee8054e64d01444d0ee55c3294c46a
Mauricio Degregori <degregori2@gmail.com>
2019-12-02 15:04:01 -0600

    got initial api running

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send({api: 'If this prints out, API is running'})
+})
+
+
+const port = 5000;
+server.listen(port, () => 
+console.log(`\n ** API running on port: ${port} **\n`)
+);
