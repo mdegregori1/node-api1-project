eccadc920cafb969a404df40258b69bea0a6cf19
Mauricio Degregori <degregori2@gmail.com>
2019-12-02 16:44:09 -0600

    post for users working

--- index.js
+++ index.js
@@ -13,18 +13,23 @@ server.get('/', (req, res) => {
 
 // post to /api/users
 
-// server.post('/api/users', (req, res) => {
-//     const data = req.body;
-//     db.insert(data)
-//     .then( users =>{
-//         res.status(201).json(users);
-//     })
-//     .catch( error => {
-//         console.log('error on post for users', error);
-//         res.status(500).json({errorMessage: 'jfdsflksd'})
-//     })
+server.post('/api/users', (req, res) => {
+    const data = req.body;
+    if (data.name && data.bio){
+        db.insert(data)
+        .then( users => {
+            res.status(201).json({...users, ...data})
+        })
+        .catch(error => {
+            console.log('error on post for users', error)
+            res.status(500).json({error: "There was an error while saving the user to the database"})
+        }) 
+    } else {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user"})
+    }
 
-// })
+
+})
 
 //get from /api/users
 server.get('/api/users', (req, res) => {
